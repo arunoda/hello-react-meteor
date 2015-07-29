724efff57d6491ce8a257c223b620917856ddb69
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-07-29 23:40:34 +0530

    Remove route level subs

--- both/router.jsx
+++ both/router.jsx
@@ -1,8 +1,4 @@
 FlowRouter.route("/", {
-  subscriptions: function() {
-    var selector = {category: {$ne: "private"}};
-    this.register('posts', Meteor.subscribe('posts', selector));
-  },
   action: function() {
     ReactLayout.render(BlogLayout, {
       content: <PostList />
